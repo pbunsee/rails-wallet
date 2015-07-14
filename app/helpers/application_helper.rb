55ef113b55b5d8e0db9290745df16ec8957cced0
Jeremy Raines <jraines@gmail.com>
2015-07-14 12:15:06 -0400

    Extract helper of errors section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
       current_user = User.find(session[:user_id])
     end
   end
+
+  def errors_for(model)
+    if model.errors.any?
+      content_tag :ul do
+        model.errors.full_messages.map do |msg|
+          content_tag :li, msg
+        end.join('').html_safe
+      end
+
+    end
+  end
 end
