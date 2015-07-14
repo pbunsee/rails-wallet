69e83ae91bb5d71a6c56651ed2b524db4e773cc0
Jeremy Raines <jraines@gmail.com>
2015-07-14 10:42:02 -0400

    Add basic card creation and views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,28 @@
 class CardsController < ApplicationController
+  def index
+    @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.create(card_params)
+    if @card.valid?
+      redirect_to cards_path
+    else
+      flash[:alert] = "There was an error with your submission"
+      render :new
+    end
+  end
+
+
+  private
+
+  def card_params
+    params.require(:card).permit(:number,
+                                 :expiration_month,
+                                 :expiration_year)
+  end
 end
