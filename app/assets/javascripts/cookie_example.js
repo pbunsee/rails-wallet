518449f2c033b3b517af624ade55bd478c851caf
Jeremy Raines <jraines@gmail.com>
2015-07-15 12:09:02 -0400

    Cookie example with animation and descriptive documentready function

--- app/assets/javascripts/cookie_example.js
+++ app/assets/javascripts/cookie_example.js
@@ -1,18 +1,45 @@
 //When the user submits their name, set a cookie with that value
 //Then, when they revisit the page, display their name in
 //the greeting div
+//
+//If the user has not visited the page before, display
+//an animation, otherwise do not display the animation.
 
-$(document).ready(function() {
+function setUserNameCookie() {
+  var usersName = $('#nameTheUserTypes').val()
+  Cookies.set('name', usersName);
+}
 
+function onAnimationComplete() {
+  $('#greeting').empty();
+}
+
+function setupFormListener() {
+  $('#cookie-name').on('submit', setUserNameCookie);
+}
+
+function animateForFirstTimeVisitor(){
+  if (Cookies.get('skipAnimation') !== 'true') {
+    $('#greeting').text('Welcome!');
+    $('#greeting').animate({'font-size': '84px'}, 3000, onAnimationComplete);
+  }
+}
+
+function markUserAsHavingVisitedPage() {
+  Cookies.set('skipAnimation', true)
+}
+
+function showNameIfInCookie(){
   if (Cookies.get('name')) {
     var name = Cookies.get('name');
     $('#greeting').html(name);
   }
+}
 
-  $('#cookie-name').on('submit', function() {
-    var name = $('#cookie-name input[name=name]').val()
-    Cookies.set('name', name);
-  });
-
+$(document).ready(function() {
+  setupFormListener();
+  animateForFirstTimeVisitor();
+  markUserAsHavingVisitedPage();
+  showNameIfInCookie();
 });
 
