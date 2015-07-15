342db895b416bce3089a5baf5a2285012c5b6edb
Jeremy Raines <jraines@gmail.com>
2015-07-15 14:13:58 -0400

    Admin model with namespaced login form

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Admin::SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @admin = Admin.find_by_username( params[:username] )
+    if @admin.present?
+      if @admin.password == params[:password]
+        session[:admin_id] = @admin.id
+        raise "Success!"
+        #redirect_to admin_cards_path
+      else
+        flash[:alert] = 'Wrong password'
+        redirect_to new_admin_session_path
+      end
+    else
+      flash[:alert] = 'No admin found with that username'
+      redirect_to new_admin_session_path
+    end
+  end
+
+  def destroy
+
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714154838) do
+ActiveRecord::Schema.define(version: 20150715174041) do
+
+  create_table "admins", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "cards", force: true do |t|
     t.string   "number"
